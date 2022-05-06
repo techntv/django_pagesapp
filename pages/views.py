77e658f76c23287b3118ecf7e2e5fd895c844360
Vinh Nguyen <techntvz@gmail.com>
2022-05-06 12:46:28 +0700

    initial commit

---
+++ pages/views.py
@@ -0,0 +1,11 @@
+from re import template
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
